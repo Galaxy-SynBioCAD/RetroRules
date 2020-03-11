95e320b4255941f77c373a4a338eb9ad4349f5aa
mdulac <melchior.dulac@inra.fr>
2020-03-11 10:33:06 +0100

    updatung reamde and defaults

--- tool_RetroRules.py
+++ tool_RetroRules.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser('Python wrapper to add cofactors to generate rpSBML collection')
     parser.add_argument('-rule_type', type=str)
     parser.add_argument('-output', type=str)
-    parser.add_argument('-diameters', type=str)
+    parser.add_argument('-diameters', type=str, default='2,4,6,8,10,12,14,16')
     parser.add_argument('-output_format', type=str)
     params = parser.parse_args()
     rpTool.passRules(params.rule_type, params.output, [int(i) for i in params.diameters.split(',')], params.output_format)
